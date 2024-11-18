33c1885ef01fddbd9cd3b5e077382a2cdcdfd8e3
hoperoth-qsys <hope.roth@qsc.com>
2024-11-18 16:14:55 -0500

    add rgb leds to properties

--- properties.lua
+++ properties.lua
@@ -18,50 +18,32 @@
 --     Value = 0
 --   }
 -- )
-table.insert(
-  props,
-  {
+table.insert(props, {
     Name = "Device Type",
     Type = "enum",
-    Choices = {"Switch", "Dimmer", "Strip"},
+    Choices = {"Switch", "Dimmer", "Strip", "LED Strip"},
     Value = "Switch"
-  }
-)
-table.insert(
-  props,
-  {
-    Name = "Energy Monitoring",
-    Type = "boolean",
-    Value = false
-  }
-)
-table.insert(
-  props,
-  {
+})
+table.insert(props,
+             {Name = "Energy Monitoring", Type = "boolean", Value = false})
+table.insert(props, {
     Name = "Number Of Outputs",
     Type = "integer",
     Min = 2,
     Max = 6,
     Value = 2
-  }
-)
-table.insert(
-  props,
-  {
+})
+table.insert(props, {
     Name = "Poll Time",
     Comment = "Time in seconds between polling the devices",
     Type = "integer",
     Min = 1,
     Max = 60,
     Value = 5
-  }
-)
-table.insert(
-  props,
-  {
+})
+table.insert(props, {
     Name = "Debug Print",
     Type = "enum",
     Choices = {"None", "Tx/Rx", "Tx", "Rx", "Function Calls", "All"},
     Value = "None"
-  }
-)
+})
--- rectify_properties.lua
+++ rectify_properties.lua
@@ -1,5 +1,6 @@
-if props.plugin_show_debug.Value == false then 
-  props["Debug Print"].IsHidden = true 
+if props.plugin_show_debug.Value == false then
+    props["Debug Print"].IsHidden = true
 end
-props["Energy Monitoring"].IsHidden = props["Device Type"].Value == "Dimmer" 
-props["Number Of Outputs"].IsHidden = props["Device Type"].Value ~= "Strip"
+props["Energy Monitoring"].IsHidden = props["Device Type"].Value == "Dimmer"
+props["Number Of Outputs"].IsHidden = props["Device Type"].Value ~= "Strip" or
+                                          "LED Strip"
